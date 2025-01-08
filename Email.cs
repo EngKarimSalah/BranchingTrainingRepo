cdcbab7efbc12dac8c6915d86c317a004cec06a6
karim <karim@gmail.com>
2025-01-08 13:28:25 +0400

    adding email class

---
+++ Email.cs
@@ -0,0 +1,14 @@
+﻿namespace School
+{
+    public class Email
+    {
+
+        public string TOaddress { get; set; }
+
+        public string FromAddress { get; set; }
+
+        public string subject { get; set; }
+
+        public string body { get; set; }
+    }
+}
